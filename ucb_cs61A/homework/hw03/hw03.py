e9c6e8e934861e59d7ad1cff4e92e0ec609d5fc9
tavaresdong <tavares.nju@gmail.com>
2016-01-08 03:45:26 -0800

    hanoi done

--- ucb_cs61A/homework/hw03/hw03.py
+++ ucb_cs61A/homework/hw03/hw03.py
@@ -188,6 +188,14 @@ def move_stack(n, start, end):
     """
     assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
     "*** YOUR CODE HERE ***"
+    if n == 1:
+        print_move(start, end)
+        return
+    handle = 6 - start - end
+    move_stack(n - 1, start, handle)
+    print_move(start, end)
+    move_stack(n - 1, handle, end)
+    return
 
 
 from operator import sub, mul
