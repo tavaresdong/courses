708f92f9f67191700b5ca4d9edbe7212550dda2a
tavaresdong <tavares.nju@gmail.com>
2016-02-04 21:16:30 +0800

    Princeton Algorithm: percolation

---
+++ princeton_algorithm/Algorithms/src/percolation/Percolation.java
@@ -0,0 +1,93 @@
+package percolation;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private int size;
+    private boolean[][] arr;
+    private WeightedQuickUnionUF uf, ufFull;
+    private int top, bottom;
+    
+    
+    public Percolation(int N) 
+    {
+        if (N <= 0) 
+            throw new IllegalArgumentException();
+        size = N;
+        top = N * N;
+        bottom = N * N + 1;
+        
+        arr = new boolean[N][N];
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        ufFull = new WeightedQuickUnionUF(N * N + 1);
+    }
+    
+    public void open(int i, int j) 
+    {
+        if (i < 1 || size < i || j < 1 || size < j)
+            throw new IndexOutOfBoundsException();
+        if (isOpen(i, j)) return;
+        arr[i - 1][j - 1] = true;
+        
+        int index = getIndex(i, j);
+        if (i == 1) {
+            uf.union(index, top);
+            ufFull.union(index, top);
+        }
+        if (i == size) {
+            uf.union(index, bottom);
+        }
+        tryUnion(i, j, i - 1, j);
+        tryUnion(i, j, i + 1, j);
+        tryUnion(i, j, i, j - 1);
+        tryUnion(i, j, i, j + 1);
+    }
+    
+    // Try to see if neighbor is valid and open, then union these two nodes
+    private void tryUnion(int curX, int curY, int neiX, int neiY) 
+    {
+        // Check four possible neighbors
+        if (0 < neiX && neiX <= size && 0 < neiY && neiY <= size) {
+            if (isOpen(neiX, neiY)) {
+                uf.union(getIndex(curX, curY), 
+                         getIndex(neiX, neiY));
+                ufFull.union(getIndex(curX, curY),
+                             getIndex(neiX, neiY));
+            }
+        }
+    }
+    
+    private int getIndex(int i, int j) 
+    {
+        return (i - 1) * size + (j - 1);
+    }
+    
+    public boolean isOpen(int i, int j) 
+    {
+        if (i < 1 || size < i || j < 1 || size < j)
+            throw new IndexOutOfBoundsException();
+        
+        return arr[i - 1][j - 1];
+    }
+    
+    public boolean isFull(int i, int j) 
+    {
+        if (i < 1 || size < i || j < 1 || size < j)
+            throw new IndexOutOfBoundsException();
+        if (isOpen(i, j) && ufFull.connected(getIndex(i, j), top))
+            return true;
+        return false;
+        
+    }
+    
+    public boolean percolates() 
+    {
+        return uf.connected(top, bottom);
+    }
+    
+    
+    public static void main(String[] args) 
+    {
+        
+    }
+
+}
---
+++ princeton_algorithm/Algorithms/src/percolation/PercolationStats.java
@@ -0,0 +1,83 @@
+package percolation;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private int round;
+    private int size;
+    private double[] fractions = null;
+    
+    private double meanVal;
+    private double stddevVal;
+
+    public PercolationStats(int N, int T) 
+    {
+        if (N <= 0 || T <= 0) 
+            throw new IllegalArgumentException();
+        round = T;
+        size = N;
+        fractions = new double[round];
+        
+        for (int i = 0; i < T; i++) {
+            fractions[i] = doSimulation();
+        }
+        
+        // Order cannot change
+        meanVal = StdStats.mean(fractions);
+        stddevVal = StdStats.stddev(fractions);
+    }
+    
+    private double doSimulation()
+    {
+        Percolation percolation = new Percolation(size);
+        int numOpen = 0;
+        while (!percolation.percolates()) {
+            int r = StdRandom.uniform(1, size + 1);
+            int c = StdRandom.uniform(1, size + 1);
+            while (percolation.isOpen(r, c)) {
+                r = StdRandom.uniform(1, size + 1);
+                c = StdRandom.uniform(1, size + 1);
+            }
+            percolation.open(r, c);
+            numOpen++;
+        }
+        
+        return ((double) numOpen) / ((double) size * size);
+    }
+    
+    public double mean()
+    {
+        
+        return meanVal;
+    }
+    
+    public double stddev()
+    {
+        return stddevVal;
+    }
+    
+    public double confidenceLo()
+    {
+        return meanVal - (1.96 * stddevVal) / Math.sqrt(round);
+    }
+    
+    public double confidenceHi()
+    {
+        return meanVal + (1.96 * stddevVal) / Math.sqrt(round);
+    }
+    
+    public static void main(String[] args)
+    {
+        if (args.length != 2)
+            throw new IllegalArgumentException();
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        
+        PercolationStats percStat = new PercolationStats(N, T);
+        StdOut.println("mean                    = " + percStat.mean());
+        StdOut.println("stddev                  = " + percStat.stddev());
+        StdOut.println("95% confidence interval = " 
+        + percStat.confidenceLo() + ", " + percStat.confidenceHi());
+    }
+}
